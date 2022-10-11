f82494e93ee09451d2262d5cfabb9051b4e97cf7
DESKTOP-QGSO4V9\Esdras <moralesespina92@gmail.com>
2022-10-10 19:02:33 -0600

    001 - CRUD de Vehiculos

--- src/app.js
+++ src/app.js
@@ -3,17 +3,25 @@ import morgan from "morgan";
 // Import Routes
 import vehiclesRoutes from "./routes/vehicles.routes"
 
+var cors = require('cors')
 const app=express();
-
 
 //Settings
 app.set('port', process.env.PORT || 3000);
 
-
 //Middlewares
+app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors())
 
 //Use Routes
+
 app.use('/api/vehicles',vehiclesRoutes);
+
+app.use((req,res,next) => {
+    res.status(404).json({
+        message: 'Ruta invalida'
+    })
+})
 
 export default app;
