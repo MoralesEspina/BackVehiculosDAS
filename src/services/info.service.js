4f1ee5aafbcda466c8bb0241312c9fcefe6d7d44
DESKTOP-QGSO4V9\Esdras <moralesespina92@gmail.com>
2022-10-11 21:52:50 -0600

    Endpoint para Menus Desplegables

---
+++ src/services/info.service.js
@@ -0,0 +1,37 @@
+const InfoStorage = require("../storage/info.storage")
+
+//TODO OBTENER TODOS LOS TIPOS
+const getAllTypes = async () => {
+    try{
+        const allTypes = await InfoStorage.getAllTypes();
+        return allTypes;
+    } catch (error){
+        throw error;
+    }   
+}
+
+//TODO OBTENER TODOS LOS TRABAJOS
+const getAllJobs = async () => {
+    try{
+        const allJobs = await InfoStorage.getAllJobs();
+        return allJobs;
+    } catch (error){
+        throw error;
+    }   
+}
+
+//TODO OBTENER TODOS LOS Estatus
+const getAllStatus = async () => {
+    try{
+        const allStatus = await InfoStorage.getAllStatus();
+        return allStatus;
+    } catch (error){
+        throw error;
+    }   
+}
+
+module.exports = {
+    getAllTypes,
+    getAllJobs,
+    getAllStatus,
+}
---
+++ src/storage/info.storage.js
@@ -0,0 +1,43 @@
+import {getConnection} from "./../database/database";
+
+//TODO OBTENER TODOS LOS TIPOS
+const getAllTypes= async () =>{
+    try{
+        const connection = await getConnection();
+        const result = await connection.query("SELECT idvtype,type_name from vtype")
+        var data=JSON.parse(JSON.stringify(result))
+        return data;
+    }catch(error){
+        throw error;
+    }
+}
+
+//TODO OBTENER TODOS LOS TRABAJOS
+const getAllJobs= async () =>{
+    try{
+        const connection = await getConnection();
+        const result = await connection.query("SELECT id,job_name,description from job")
+        var data=JSON.parse(JSON.stringify(result))
+        return data;
+    }catch(error){
+        throw error;
+    }
+}
+
+//TODO OBTENER TODOS LOS Estatus
+const getAllStatus= async () =>{
+    try{
+        const connection = await getConnection();
+        const result = await connection.query("SELECT idstatus,status_name,description from status")
+        var data=JSON.parse(JSON.stringify(result))
+        return data;
+    }catch(error){
+        throw error;
+    }
+}
+
+module.exports = {
+    getAllTypes,
+    getAllJobs,
+    getAllStatus,
+}
